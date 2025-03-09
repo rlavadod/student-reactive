427d9574d4ac06c62b875f99bbe53c59ed1923b5
ruddylavado <rlavadod@gmail.com>
2025-03-09 18:07:29 -0500

    feat: add valid annotation

--- src/main/java/com/encora/studentreactive/controller/StudentController.java
+++ src/main/java/com/encora/studentreactive/controller/StudentController.java
@@ -1,14 +1,12 @@
 package com.encora.studentreactive.controller;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import com.encora.studentreactive.model.api.Student;
 import com.encora.studentreactive.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
--- src/main/java/com/encora/studentreactive/model/api/Student.java
+++ src/main/java/com/encora/studentreactive/model/api/Student.java
@@ -1,18 +1,32 @@
 package com.encora.studentreactive.model.api;
 
 import com.encora.studentreactive.model.entity.StudentEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Builder
 @Getter
 @Setter
 public class Student {
+
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private Long id;
+  @NotBlank
+  @Pattern(regexp = "^[a-zA-Z]+")
   private String firstname;
+  @NotBlank
+  @Pattern(regexp = "^[a-zA-Z]+")
   private String lastname;
   private Boolean status;
+  @Min(0)
+  @Max(130)
   private Integer age;
 
   public static Student from(StudentEntity entity) {
